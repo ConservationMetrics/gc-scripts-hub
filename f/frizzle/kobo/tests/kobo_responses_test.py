cfde803faabc211f1ca6afc4ffd8a2e61e4f6ce2
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-12-05 14:31:23 -0600

    Add end-to-end test for kobo_responses

--- f/frizzle/kobo/tests/kobo_responses_test.py
+++ f/frizzle/kobo/tests/kobo_responses_test.py
@@ -1,4 +1,10 @@
-from f.frizzle.kobo.kobo_responses import sanitize
+import re
+from dataclasses import dataclass
+
+import pytest
+
+from f.frizzle.kobo.kobo_responses import main, sanitize
+from f.frizzle.kobo.tests import kobo_form
 
 
 def test_sanitize():
@@ -11,7 +17,9 @@ def test_sanitize():
     }
     global_mapping = {"x": "X"}
 
-    sql_message, updated_global_mapping = sanitize(message, global_mapping, "/", [("/", "__")])
+    sql_message, updated_global_mapping = sanitize(
+        message, global_mapping, "/", [("/", "__")]
+    )
     assert sql_message == {
         "col1": 1,
         "col2": 2,
@@ -77,3 +85,59 @@ def test_sanitize_with_nesting():
         "group1": '{"group2": {"question": "How ya doin?"}}',
         "url": "gopher://example.net",
     }
+
+
+@pytest.fixture
+def koboserver(mocked_responses):
+    """A mock Kobo Server that you can use to provide survey responses"""
+
+    @dataclass
+    class KoboServer:
+        account: dict
+        form_id: str
+
+    server_url = "http://kobo.example.org"
+    form_id = "mimsyweretheborogoves"
+    form_name = "Arboles"
+
+    mocked_responses.get(
+        f"{server_url}/api/v2/assets/{form_id}/",
+        json=kobo_form.kobo_form(server_url, form_id, form_name),
+        status=200,
+    )
+    mocked_responses.get(
+        f"{server_url}/api/v2/assets/{form_id}/data/",
+        json=kobo_form.kobo_form_submissions(server_url, form_id),
+        status=200,
+    )
+    mocked_responses.get(
+        re.compile(rf"{server_url}/api/v2/assets/{form_id}/data/\d+/attachments/\d+/?"),
+        body=open("f/frizzle/kobo/tests/trees.png", "rb").read(),
+        content_type="image/png",
+        headers={"Content-Length": "3632"},
+    )
+
+    return KoboServer(dict(server_url=server_url, api_key="Callooh!Callay!"), form_id)
+
+
+@pytest.fixture
+def pg_database():
+    return dict(dbname="", user="", password="", host="", port="")
+
+
+def test_script(koboserver, pg_database, tmp_path):
+    attachments = tmp_path / "attachments"
+
+    main(
+        koboserver.account, koboserver.form_id, pg_database, "kobo_responses", tmp_path
+    )
+
+    # Attachments are saved to disk
+    assert (
+        attachments
+        / "cmi_admin_kobo_test/attachments/f7bef041e8624f09946bff05ee5cbd4b/5c408d9d-6a76-4fbb-bf4e-9ed8f8e3e382/1637241249813.jpg"
+    ).exists()
+
+    # # Survey responses are written to a SQL Table
+    # engine = sqlalchemy.create_engine(f"sqlite:///{warehousedb}")
+    # assert sqlalchemy.inspect(engine).has_table("kobo_responses")
