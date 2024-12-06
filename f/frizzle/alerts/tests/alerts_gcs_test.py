d8c7e05353ebf8f2e9a33c3679b8554054cf4106
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-12-05 20:25:37 -0600

    Get GCS emulator running as a Pytest fixture

--- f/frizzle/alerts/tests/alerts_gcs_test.py
+++ f/frizzle/alerts/tests/alerts_gcs_test.py
@@ -1,10 +1,21 @@
-from f.frizzle.alerts.alerts_gcs import main
+import pytest
+
+from f.frizzle.alerts.alerts_gcs import _main
 
 
-def test_script(pg_database, tmp_path):
+@pytest.fixture
+def mock_alerts_storage_client(gcs_service_client):
+    yield gcs_service_client
+
+
+def test_script(pg_database, mock_alerts_storage_client, tmp_path):
     asset_storage = tmp_path / "datalake"
 
-    main(
+    _main(
+        mock_alerts_storage_client,
+        "gcp-bucket",
+        0,
         pg_database,
+        "fake_alerts",
         asset_storage,
     )
