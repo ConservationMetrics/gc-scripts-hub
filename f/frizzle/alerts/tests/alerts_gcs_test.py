a5f500e09927dafd7eb3cb5167cef295029fea1d
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-12-09 11:07:57 -0600

    Check-in test assets

--- f/frizzle/alerts/tests/alerts_gcs_test.py
+++ f/frizzle/alerts/tests/alerts_gcs_test.py
@@ -23,48 +23,24 @@ def mock_alerts_storage_client(gcs_emulator_client):
         logger.warning("Bucket already exists. Attempting to get existing bucket.")
         bucket = storage_client.bucket(MOCK_BUCKET_NAME)
 
-    def upload_blob(bucket, source_file_name, destination_blob_name):
+    def _upload_blob(bucket, source_file_name, destination_blob_name):
         blob = bucket.blob(destination_blob_name)
         blob.upload_from_filename(source_file_name)
         logger.info(f"Uploaded {source_file_name} -> {destination_blob_name}")
 
-    def upload_text_blob(bucket, s, destination_blob_name):
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_string(s)
-        logger.info(f"Uploaded text blob -> {destination_blob_name}")
-
-    def verify_blob_exists(bucket, blob_name):
-        blob = bucket.blob(blob_name)
-        return blob.exists()
-
     # Upload test files to the emulator
+    assets_directory = "f/frizzle/alerts/tests/assets/"
     alerts_filenames = [
         "alerts_history.csv",
+        "100/vector/2023/09/alerts.geojson",
         "100/raster/2023/09/S1_T0_202309900112345671.tif",
         "100/raster/2023/09/S1_T1_202309900112345671.tif",
         "100/raster/2023/09/S2_T0_202309900112345671.tif",
         "100/raster/2023/09/S2_T1_202309900112345671.tif",
     ]
-    assets_directory = "f/frizzle/alerts/tests/assets/"
-
     for filename in alerts_filenames:
         source_path = os.path.join(assets_directory, os.path.basename(filename))
-        upload_blob(bucket, source_path, filename)
-        assert verify_blob_exists(
-            bucket, filename
-        ), f"Blob {filename} was not uploaded successfully"
-
-    # # Upload geojson alert
-    # geojson_data = json.dumps(incoming_geojson_alert.incoming_geojson_alert())
-    # geojson_blob_name = "100/vector/2023/09/incoming_alert.geojson"
-    # try:
-    #     upload_string_blob(bucket, geojson_data, geojson_blob_name)
-    #     assert verify_blob_exists(
-    #         bucket, geojson_blob_name
-    #     ), f"Blob {geojson_blob_name} was not uploaded successfully"
-    # except Exception as e:
-    #     logger.error(f"Failed to upload or verify {geojson_blob_name}: {e}")
-    #     raise
+        _upload_blob(bucket, source_path, filename)
 
     yield storage_client
 
@@ -85,15 +61,22 @@ def test_script_e2e(pg_database, mock_alerts_storage_client, tmp_path):
     with psycopg2.connect(**pg_database) as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT COUNT(*) FROM fake_alerts")
-            assert cursor.fetchone()[0] == 3
+            assert cursor.fetchone()[0] == 1  # Length of assets/alerts.geojson
 
+            cursor.execute("SELECT COUNT(*) FROM fake_alerts__metadata")
+            assert cursor.fetchone()[0] == 5  # Length of asserts/alerts_history.csv
+
+    # Metadata is saved to disk
+    assert (asset_storage / "alerts_history.csv").exists()
+    assert (asset_storage / "100/vector/2023/09/alerts.geojson").exists()
+
+    # Rasters are saved to disk
     for basename in (
         "S1_T0_202309900112345671",
         "S1_T1_202309900112345671",
         "S2_T0_202309900112345671",
         "S2_T1_202309900112345671",
     ):
-        # Attachments are saved to disk
         # file naming format: <territory_id>/<year_detec>/<month_detec>/<alert_id>/images/<filename>.tif
         assert (
             asset_storage / "100/2023/09/202309900112345671/images" / f"{basename}.tif"
