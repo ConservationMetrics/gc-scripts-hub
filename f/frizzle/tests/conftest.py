bac1c9260f604743b90c333393774012a2234c5a
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-12-09 10:29:04 -0600

    Pull imports into shared fixtures to avoid ImportError

--- f/frizzle/tests/conftest.py
+++ f/frizzle/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
-import responses
-import testing.postgresql
 
 
 @pytest.fixture
 def mocked_responses():
     """responses.RequestsMock context, for testing code that makes HTTP requests."""
+    import responses
+
     with responses.RequestsMock() as rsps:
         yield rsps
 
@@ -13,6 +13,8 @@ def mocked_responses():
 @pytest.fixture
 def pg_database():
     """A dsn that may be used to connect to a live (local for test) postgresql server"""
+    import testing.postgresql
+
     db = testing.postgresql.Postgresql(port=7654)
     dsn = db.dsn()
     dsn["dbname"] = dsn.pop("database")
