5d93cb16429099f153ef84b725d13fc2c20a22c9
Rudo Kemper <rtakemper@gmail.com>
2024-12-18 14:45:20 -0500

    Revert conftest changes

--- f/connectors/alerts/tests/conftest.py
+++ f/connectors/alerts/tests/conftest.py
@@ -20,7 +20,7 @@ def pg_database():
 def gcs_emulator_client():
     """Return a google.cloud.storage.Client against an GCS emulator running in tox-docker"""
     # See https://github.com/tox-dev/tox-docker?tab=readme-ov-file#configuration, `expose` option
-    TOX_DOCKER_GCS_PORT = os.environ.get("TOX_DOCKER_GCS_PORT", "4443")
+    TOX_DOCKER_GCS_PORT = os.environ["TOX_DOCKER_GCS_PORT"]
     emulator_endpoint = f"http://localhost:{TOX_DOCKER_GCS_PORT}"
 
     # Create a storage client that connects to the emulator
