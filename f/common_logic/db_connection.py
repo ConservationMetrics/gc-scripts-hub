f5137a0c6e2b85afa8373f764e7080cd8db87ce8
Rudo Kemper <rtakemper@gmail.com>
2025-02-12 15:53:49 -0500

    Set up common logic files

---
+++ f/common_logic/db_connection.py
@@ -0,0 +1,9 @@
+postgresql = dict
+
+
+def conninfo(db: postgresql):
+    """Convert a `postgresql` Windmill Resources to psycopg-style connection string"""
+    # password is optional
+    password_part = f" password={db['password']}" if "password" in db else ""
+    conn = "dbname={dbname} user={user} host={host} port={port}".format(**db)
+    return conn + password_part
