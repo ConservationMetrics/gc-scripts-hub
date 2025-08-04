cd19657479da5b4c5172fbba67ff687111ac0fc4
Rudo Kemper <rtakemper@gmail.com>
2025-08-04 16:31:36 -0400

    Resolve database name

--- f/apps/gc_dataset_importer.app/0_form_stepper_validations.inline_script.frontend.js
+++ f/apps/gc_dataset_importer.app/0_form_stepper_validations.inline_script.frontend.js
@@ -1,14 +1,16 @@
-// Step 1 validation
-if (formStepper.currentStepIndex === 0 && !state.datasetAvailable) {
+const { currentStepIndex, lastAction } = formStepper;
+
+// Step 1: Dataset name must be valid
+if (currentStepIndex === 0 && !state.datasetAvailable) {
   throw new Error("Please enter a valid dataset name to proceed.");
 }
 
-// Step 2 validation
-if (formStepper.currentStepIndex === 1 && formStepper.lastAction === "next" && !state.uploadSuccess) {
+// Step 2: File must be uploaded
+if (currentStepIndex === 1 && lastAction === "next" && !state.uploadSuccess) {
   throw new Error("Please upload your file to proceed.");
 }
 
-// Step 4 validation
-if (formStepper.currentStepIndex === 3 && state.finalizeSuccess === true) {
-  throw new Error("Please refresh the page to upload another file.")
+// Step 4: Can't reuse same session
+if (currentStepIndex === 3 && state.finalizeSuccess) {
+  throw new Error("Please refresh the page to upload another file.");
 }
--- f/apps/gc_dataset_importer.app/1_check_if_database_table_exists.inline_script.py
+++ f/apps/gc_dataset_importer.app/1_check_if_database_table_exists.inline_script.py
@@ -4,5 +4,11 @@ from f.common_logic.db_transformations import force_valid_sql_name
 
 def main(db: postgresql, dataset_name: str):
     valid_sql_name = force_valid_sql_name(dataset_name)
+
     table_exists = check_if_table_exists(conninfo(db), valid_sql_name)
-    return {"tableExists": table_exists, "datasetName": dataset_name, "validSqlName": valid_sql_name}
+
+    return {
+        "tableExists": table_exists,
+        "datasetName": dataset_name,
+        "validSqlName": valid_sql_name,
+    }
