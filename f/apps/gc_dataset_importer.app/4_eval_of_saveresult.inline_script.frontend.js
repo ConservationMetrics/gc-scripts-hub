74204115b0cb0331cf855da4438c173571af9d1e
Rudo Kemper <rtakemper@gmail.com>
2025-08-05 10:42:41 -0400

    UI improvements

--- f/apps/gc_dataset_importer.app/4_eval_of_saveresult.inline_script.frontend.js
+++ f/apps/gc_dataset_importer.app/4_eval_of_saveresult.inline_script.frontend.js
@@ -1,8 +1,8 @@
 if (state.finalizeSuccess === true) {
   if (state.dataSource) {
-    return `✅ File successfully transformed and written to the data warehouse!`
+    return `✅ File successfully transformed and written to the data warehouse! Reload the page to upload another dataset, or navigate to a Guardian Connector tool like Explorer to set up a view for your dataset.`
   } else {
-    return "✅ File successfully written to the data warehouse!"
+    return "✅ File successfully written to the data warehouse! Reload the page to upload another dataset, or navigate to a Guardian Connector tool like Explorer to set up a view for your dataset."
   }
 } else {
   return ""
