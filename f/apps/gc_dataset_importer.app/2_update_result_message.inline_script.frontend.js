646ac692b20bb92cae464011c989c3d10af350e9
Rudo Kemper <rtakemper@gmail.com>
2025-08-27 22:23:11 -0400

    fix: upload button is disabled if file upload successful

--- f/apps/gc_dataset_importer.app/2_update_result_message.inline_script.frontend.js
+++ f/apps/gc_dataset_importer.app/2_update_result_message.inline_script.frontend.js
@@ -1,6 +1,6 @@
-if (state.uploadButtonEnabled && state.uploadSuccess) {
+if (state.uploadSuccess) {
   return "✅ File successfully uploaded to temporary storage! Please proceed to the next step to finish writing the data to the warehouse."
-} else if (state.uploadButtonEnabled && !state.uploadSuccess && state.uploadErrorMessage) { 
+} else if (!state.uploadSuccess && state.uploadErrorMessage) { 
   return `❌ ${state.uploadErrorMessage}`
 } else {
   return ""
