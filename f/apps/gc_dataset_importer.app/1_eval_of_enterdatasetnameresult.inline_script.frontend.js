817c3a6cc9cee9158792dcd3adcab048291b7c3e
Rudo Kemper <rtakemper@gmail.com>
2026-01-11 18:49:50 -0500

    Frontend fixes

--- f/apps/gc_dataset_importer.app/1_eval_of_enterdatasetnameresult.inline_script.frontend.js
+++ f/apps/gc_dataset_importer.app/1_eval_of_enterdatasetnameresult.inline_script.frontend.js
@@ -1,7 +1,7 @@
 switch (state.tableExists) {
-  case true:
-    return `✅ New dataset! The database table "${state.validSqlName}" will be created.`;
   case false:
+    return `✅ New dataset! The database table "${state.validSqlName}" will be created.`;
+  case true:
     return `⚠️ Dataset "${state.validSqlName}" already exists. New data will be appended to the existing table.`;
   default:
     return "";
